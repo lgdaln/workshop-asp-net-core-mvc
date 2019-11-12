a32edfd214a5a9f1235cd4cccaa8879b7017f359
lgdaln <lgdaln@hotmail.com>
2019-11-12 09:00:44 -0300

    Other entities and second migration

---
+++ Models/Enums/SalesStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        Peding = 0,
+        Billed = 1,
+        Canceled = 2
+
+    }
+}
